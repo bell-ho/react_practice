df92064fbbe4bed32d0222a5c7b209a4dea87bfb
bell-ho <jj0101065@gmail.com>
2022-04-10 15:18:36 +0900

    news

--- src/components/News.js
+++ src/components/News.js
@@ -1,26 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import NewsList from './NewsList';
+import Categories from './Categories';
+import NewsPage from '../pages/NewsPage';
+import { Route } from 'react-router-dom';
 
 const News = () => {
-    const [data, setData] = useState(null);
 
-    const onClick = async () => {
-        try {
-            const res = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=84d2dd8ce0444fc3a5987c5bb0177430',);
-            setData(res.data);
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
-    return (
-        <div>
-            <div>
-                <button onClick={onClick}>클릭</button>
-            </div>
-            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-        </div>
-    );
+  return <Route path="/news/:category?" component={NewsPage} />;
 };
 
 export default News;
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
-const NewsList = () => {
-    return (
-        <div>
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and(max-width: 768) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
+};
 
-        </div>
+const NewsList = ({ category }) => {
+  const [loading, res, err] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=84d2dd8ce0444fc3a5987c5bb0177430`,
     );
+  }, [category]);
+
+  // 대기중
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+
+  // 아직 res 값 설정 안됐을 때
+  if (!res) {
+    return null;
+  }
+
+  //에러
+  if (err) {
+    return <NewsListBlock>에러 발생</NewsListBlock>;
+  }
+
+  // res 값 유효
+  const { articles } = res.data;
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
 };
 
 export default NewsList;
